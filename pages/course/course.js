879144641651d1b1b784d1bd2ddfab3ab4fb7c62
yyyyyyounger <1049825685@qq.com>
2021-06-06 12:08:35 +0800

    update

--- pages/course/course.js
+++ pages/course/course.js
@@ -1,66 +1,35 @@
-// pages/course/course.js
+// 可以只保留Page和data
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 字符串類型
+    msg:"Hello, mina !",
+    // 數字類型
+    num:10000,
+    //bool 類型
+    isGirl:false,
+    person:{
+      age:74,
+      height:147,
+      weight:200,
+      name:"富婆"
+    },
+    //數組類型
+    list:[
+      {
+        id:0,
+        name:"豬八戒"
+      },
+      {
+        id:1,
+        name:"天蓬元帥"
+      },
+      {
+        id:2,
+        name:"悟能"
+      },
+    ]
   }
 })
