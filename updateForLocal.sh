501b6b8e949b5a6dbe2f2122106a66ab61e78386
yyyyyyounger <yyyyyyounger@qq.com>
2021-06-08 12:37:10 +0800

    update

--- updateForLocal.sh
+++ updateForLocal.sh
@@ -2,3 +2,8 @@ echo "----------------- Update for local ---------------------"
 echo " "
 
 git pull -f origin master
+
+echo " "
+echo "The program has finished ~"
+echo " "
+read -p "Press \"Enter\" to End"
--- updateToGithub.sh
+++ updateToGithub.sh
@@ -7,6 +7,7 @@ git commit -m "update"
 
 git push -f origin master
 
+echo " "
 echo "The program has finished ~"
 echo " "
 read -p "Press \"Enter\" to End"
