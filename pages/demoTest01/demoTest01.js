33207265e45734f6a2522d318603c98007710752
yyyyyyounger <1049825685@qq.com>
2021-06-08 12:30:29 +0800

    update

---
+++ pages/demoTest01/demoTest01.js
@@ -0,0 +1,25 @@
+// pages/demoTest01/demoTest01.js
+Page({
+  data: {
+    num:0
+  },
+  // 輸入框的input事件的執行邏輯
+  handleInput (e) {   // e表示event源對象
+    //console.log("😁");      // 用於在Console輸出相關信息
+    console.log(e.detail.value);
+    this.setData({
+      num:e.detail.value
+    })
+  },
+  
+  // 加減按鈕的事件
+  handleTap (e) {
+    console.log(e);
+    const operation = e.currentTarget.dataset.operation;
+    this.setData({
+      // 需要用 this. 獲取值
+      num: this.data.num + operation
+    })
+  }
+  
+})
