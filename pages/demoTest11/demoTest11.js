8e159adfe2e056693547049a9d5f15e4976c24de
yyyyyyounger <yyyyyyounger@qq.com>
2021-06-12 15:16:38 +0800

    update

--- pages/demoTest11/demoTest11.js
+++ pages/demoTest11/demoTest11.js
@@ -1,15 +1,49 @@
 // pages/demoTest11/demoTest11.js
 Page({
   data: {
-    gender: ""
+    gender: "",
+    list:[
+      {
+        id:0,
+        name:"苹果",
+        value:"apple",
+      },
+      {
+        id:1,
+        name:"葡萄",
+        value:"grape",
+      },
+      {
+        id:2,
+        name:"香蕉",
+        value:"banana",
+      }
+    ],
+    checkedList:[]
   },
+
   handleChange(e){
     console.log(e);
     // 獲取選擇的單選框的值
-    let gender = e.detail.value;
+    let genderLog = e.detail.value;
+    if(genderLog == 'female'){
+      genderLog = '女'
+    }
+    else{
+      genderLog = '男'
+    }    
     // 把值賦值給 data中的數據
     this.setData({
-      gender:gender
+      gender:genderLog
+    })
+  },
+
+  //複選框的選中事件
+  handleItemChange(e){
+    // console.log(e.detail.value);
+    const checkedList = e.detail.value;
+    this.setData({
+      checkedList:checkedList
     })
   }
   
